d4551a786998f5ab9ca0c714145d6412d44fbc69
rdyjun <rdyjun00@gmail.com>
2024-12-26 20:22:16 +0900

    feat: swagger 초기화

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('Baekjoon testcase API')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .addServer('http://localhost:3000/api', 'Local environment')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api-docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
