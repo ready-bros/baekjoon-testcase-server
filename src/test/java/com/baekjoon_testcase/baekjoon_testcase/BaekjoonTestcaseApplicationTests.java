af2efac591c527443091bd0069274fd4320f20c6
rdyjun <rdyjun00@gmail.com>
2025-02-07 15:23:09 +0900

    init: 프로젝트 초기화

---
+++ src/test/java/com/baekjoon_testcase/baekjoon_testcase/BaekjoonTestcaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baekjoon_testcase.baekjoon_testcase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaekjoonTestcaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
