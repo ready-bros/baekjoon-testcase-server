b5d3b1de4904ca5c1f74907442100eadaeaf6685
rdyjun <rdyjun00@gmail.com>
2025-02-16 01:19:54 +0900

    refactor: Language enum 확장자 추가

--- src/main/java/com/baekjoon_testcase/baekjoon_testcase/common/Language.java
+++ src/main/java/com/baekjoon_testcase/baekjoon_testcase/common/Language.java
@@ -1,5 +1,17 @@
 package com.baekjoon_testcase.baekjoon_testcase.common;
 
+import lombok.Getter;
+
+@Getter
 public enum Language {
-    NODE
+    NODE("node", ".js")
+    ;
+
+    private String name;
+    private String fileExtension;
+
+    Language(String name, String fileExtension) {
+        this.name = name;
+        this.fileExtension = fileExtension;
+    }
 }
