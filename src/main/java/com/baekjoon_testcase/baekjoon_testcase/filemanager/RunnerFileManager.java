7f9db7c827821415ef9538cc795762105351c090
rdyjun <rdyjun00@gmail.com>
2025-02-22 10:45:41 +0900

    refactor: output 저장 로직 제거

--- src/main/java/com/baekjoon_testcase/baekjoon_testcase/filemanager/RunnerFileManager.java
+++ src/main/java/com/baekjoon_testcase/baekjoon_testcase/filemanager/RunnerFileManager.java
@@ -2,5 +2,4 @@ package com.baekjoon_testcase.baekjoon_testcase.filemanager;
 
 public interface RunnerFileManager {
     void saveCode(long id, String code, String extension);
-    void saveOutput(long id, String output);
 }
--- src/main/java/com/baekjoon_testcase/baekjoon_testcase/filemanager/RunnerFileManagerImpl.java
+++ src/main/java/com/baekjoon_testcase/baekjoon_testcase/filemanager/RunnerFileManagerImpl.java
@@ -10,8 +10,6 @@ import org.springframework.stereotype.Component;
 public class RunnerFileManagerImpl implements RunnerFileManager {
     @Value("${folder.name.code}")
     private String CODE_FOLDER_NAME;
-    @Value("${folder.name.output}")
-    private String OUTPUT_FOLDER_NAME;
     @Value("${file.name}")
     private String FILE_NAME;
 
@@ -26,18 +24,6 @@ public class RunnerFileManagerImpl implements RunnerFileManager {
             Files.writeString(Path.of(codeDirectory, this.FILE_NAME + extension), code);
         } catch (IOException e) {
             throw new RuntimeException("임시 코드 저장 예외", e);
-        }
-    }
-
-    @Override
-    public void saveOutput(long id, String output) {
-        try {
-            if (!Files.exists(Path.of(this.OUTPUT_FOLDER_NAME))) {
-                Files.createDirectory(Path.of(this.OUTPUT_FOLDER_NAME));
-            }
-            Files.writeString(Path.of(this.OUTPUT_FOLDER_NAME + "/" + id + ".txt"), output);
-        } catch (IOException e) {
-            throw new RuntimeException("출력 결과 저장 예외", e);
         }
     }
 
